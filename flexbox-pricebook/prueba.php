b243c4db1ef9968924d591fbfecf90867bc227d7
zimael91 <david.f.r91@hotmail.com>
2016-09-11 17:25:38 -0500

    ejercicio 1 y 2
    cuento con el ejercicio 1 osea el archivo index.php que me indica las
    propiedades de flexbox y cuento con el ejercicio 2 de flexbox en el que
    trato de maquetar la pagina web pricebook.

---
+++ flexbox-pricebook/prueba.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+<link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+</head>
+<body>
+
+<div class="contenedorglobal">
+	
+	<div id="barra">
+		
+
+			<div id="contenedorlogo">
+				
+				<div class="logo">
+		              
+		              PR$CE BOOK
+
+		        </div>
+
+		        <div id="contenedoropcionlogin">
+		        	     
+		            Inisiar Sesion 
+
+		        </div>
+
+			
+			</div>
+
+	</div>
+
+	<div id="contenedorinfo">
+		 
+		 <div class="caja" id="caja1">
+		 	
+		 </div>
+         
+         <div class="caja" id="caja2"></div>
+       
+         <div class="caja" id="caja3"></div>
+         <div class="caja" id="caja4"></div>
+         <div class="caja" id="caja5"></div>
+
+
+	</div>
+
+
+
+</div>
+
+
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+ <meta name="viewport" content="width=device-width, initial-scale=1	 ">
+
+ <link rel="stylesheet" href="estilo.css">
+
+</head>
+<body>
+
+
+<div id="contenedor">
+	
+ <div class="elemento" id="elemento1">1</div>
+ 	<div class="elemento" id="elemento2">2</div>
+ 	<div class="elemento" id="elemento3">3</div>	
+    <div class="elemento" id="elemento4">4</div>
+    <div class="elemento" id="elemento5">5</div>
+ <div class="elemento" id="elemento6">6</div>
+ <div class="elemento" id="elemento7">7</div>
+ <div class="elemento" id="elemento8">8</div>
+
+ <div class="elemento" id="elemento9">9</div>
+ <div class="elemento" id="elemento10">10</div>
+ <div class="elemento" id="elemento11">11</div>
+
+
+</div>
+
+
+	
+</body>
+</html>
